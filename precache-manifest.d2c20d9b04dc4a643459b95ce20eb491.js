cd71a8420f203008f2385099f306d65fcfcb01e6
hyongshu <hyongshu@gmail.com>
2020-02-24 18:01:18 +0700

    Updates

---
+++ precache-manifest.d2c20d9b04dc4a643459b95ce20eb491.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a394e2b638b7c8f4930e8fc42332d358",
+    "url": "/pwa/index.html"
+  },
+  {
+    "revision": "2a6aac25aab3be245e80",
+    "url": "/pwa/static/css/main.c42a7d1d.chunk.css"
+  },
+  {
+    "revision": "968f4fe7610b5f0d92c7",
+    "url": "/pwa/static/js/2.5a343337.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/pwa/static/js/2.5a343337.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a6aac25aab3be245e80",
+    "url": "/pwa/static/js/main.5a7cc52e.chunk.js"
+  },
+  {
+    "revision": "51332a315a9fcdd21569",
+    "url": "/pwa/static/js/runtime-main.0dbbb068.js"
+  },
+  {
+    "revision": "54339eb5d4785e775ad01deee4ef57a6",
+    "url": "/pwa/static/media/chart.54339eb5.png"
+  },
+  {
+    "revision": "27306f281900672d373bcb068621f386",
+    "url": "/pwa/static/media/logo_yamaha.27306f28.png"
+  }
+]);
